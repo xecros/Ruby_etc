1aab33fdf90f3834307f0a894231979ee48cf1b6
xecros <92353511+xecros@users.noreply.github.com>
2022-01-27 09:52:57 +0900

    Delete picture_exjpg.rb

--- picture_exjpg.rb
+++
@@ -1,6 +0,0 @@
-Dir.glob("*/*.png") do |name|
-    File.rename(name,"#{File.dirname(name)}/#{File.basename(name,".png")}.jpg")
-end
-Dir.glob("*.png") do |name|
-    File.rename(name,"#{File.basename(name,".png")}.jpg")
-end
